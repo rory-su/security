967b0a391b41061a6aef069143cc5dfa1fb935ec
jqsu <136471676@qq.com>
2019-05-27 11:26:33 +0800

    git测试

--- security-demo/src/main/java/com/rory/TestController.java
+++ security-demo/src/main/java/com/rory/TestController.java
@@ -1,10 +1,13 @@
 package com.rory;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
+
+    @RequestMapping("/test")
     public String test(){
-        return "sujinquan";
+        return "sujinquan love juan";
     }
 }

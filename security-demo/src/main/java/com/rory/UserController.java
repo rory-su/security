65a3f542e3a56f1464c6e518aafaeae56f820a88
jqsu <136471676@qq.com>
2019-05-27 10:49:45 +0800

    rory-su

--- security-demo/src/main/java/com/rory/UserController.java
+++ security-demo/src/main/java/com/rory/UserController.java
@@ -1,16 +1,25 @@
 package com.rory;
 
 
+import com.rory.bean.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class UserController {
 
     @RequestMapping(value = "/user",method = RequestMethod.GET)
-    public String query(){
-       return "sujiqnuan love juan";
+    public List<User> query(){
+       List<User> list= new ArrayList<User>();
+       list.add(new User());
+       list.add(new User());
+       list.add(new User());
+       list.add(new User());
+       return list;
     }
 }
--- security-demo/src/test/java/com/rory/DemoApplicationTest.java
+++ security-demo/src/test/java/com/rory/DemoApplicationTest.java
@@ -1,16 +1,40 @@
 package com.rory;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTest {
 
+     @Autowired
+     protected WebApplicationContext wac;
+     protected MockMvc mockMvc;
+     @Before
+     public void setup(){
+         mockMvc=MockMvcBuilders.webAppContextSetup(wac).build();
+     }
     @Test
-    public void contextLoads() {
-        System.out.println("hello word");
+    public void contextLoads() throws  Exception {
+        mockMvc.perform(get("/user")
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(3));
+
     }
+
+
 }

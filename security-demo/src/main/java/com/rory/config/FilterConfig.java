8291d87f86b8fe9f9aae7d4815946e75c3c3444a
jqsu <136471676@qq.com>
2019-05-28 17:34:29 +0800

    filter

---
+++ security-demo/src/main/java/com/rory/config/FilterConfig.java
@@ -0,0 +1,26 @@
+package com.rory.config;
+
+import com.rory.filter.MyFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.DispatcherType;
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean registFilter(){
+        FilterRegistrationBean registraion=new FilterRegistrationBean();
+        registraion.setFilter(new MyFilter());
+        registraion.addUrlPatterns("/*");
+        registraion.addInitParameter("exclusions", "/*.js,/*.gif,/*.jpg,/*.png,/*.css,/*.ico");
+        registraion.setDispatcherTypes(DispatcherType.REQUEST);
+        registraion.setName("MyFilter");
+        registraion.setOrder(1);
+        return registraion;
+    }
+}

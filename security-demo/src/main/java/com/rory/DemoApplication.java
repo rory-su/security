f488865fe725a799ee116672f10af110076e6577
jqsu <136471676@qq.com>
2019-05-29 10:09:53 +0800

    git测试

--- security-demo/src/main/java/com/rory/DemoApplication.java
+++ security-demo/src/main/java/com/rory/DemoApplication.java
@@ -1,10 +1,12 @@
 package com.rory;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.rory.dao")
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);

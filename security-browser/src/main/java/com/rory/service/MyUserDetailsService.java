1b2e6800dd471d98084aa02bdc4a0ce21afa0dbe
jqsu <136471676@qq.com>
2019-05-29 16:51:57 +0800

    git测试

--- security-browser/src/main/java/com/rory/service/MyUserDetailsService.java
+++ security-browser/src/main/java/com/rory/service/MyUserDetailsService.java
@@ -1,6 +1,5 @@
 package com.rory.service;
 
-import com.rory.dao.UserMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;

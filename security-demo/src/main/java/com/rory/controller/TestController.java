2fb93ed2c960931ffaa9d3cff0a90687ae5f4a9f
rory-su <136471676@qq.com>
2019-06-01 13:26:13 +0800

    修改配置文件

--- security-demo/src/main/java/com/rory/controller/TestController.java
+++ security-demo/src/main/java/com/rory/controller/TestController.java
@@ -4,13 +4,15 @@ import com.rory.exception.UserNotExistException;
 import org.springframework.boot.autoconfigure.web.BasicErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
 
     @RequestMapping("/test")
+    @ResponseBody
     public String test(){
-        throw  new UserNotExistException("123");
-//    return "test";
+       // throw  new UserNotExistException("123");
+        return "testee";
     }
 }

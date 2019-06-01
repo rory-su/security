29363c466fa0641eb3a49f48802741649989e28a
rory-su <136471676@qq.com>
2019-06-01 11:40:52 +0800

    修改配置文件

---
+++ security-demo/src/main/java/com/rory/bean/UserBean.java
@@ -0,0 +1,17 @@
+package com.rory.bean;
+
+import com.rory.model.User;
+
+import java.util.Date;
+
+public class UserBean extends User {
+    private Date birthday;
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}

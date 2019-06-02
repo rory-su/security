eaa982bbf4c7f53833830ea8b3d17771c1f5360d
rory-su <136471676@qq.com>
2019-06-02 22:44:36 +0800

    测试上传文件

--- security-demo/src/main/java/com/rory/config/InterceptorConfig.java
+++ security-demo/src/main/java/com/rory/config/InterceptorConfig.java
@@ -3,14 +3,26 @@ package com.rory.config;
 import com.rory.interceptor.MyInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.async.DeferredResultProcessingInterceptor;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-//@Configuration
+@Configuration
 public class InterceptorConfig extends WebMvcConfigurerAdapter {
 
     @Autowired
     private MyInterceptor myInterceptor;
+
+
+    @Override
+    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+       // super.configureAsyncSupport(configurer);
+
+        //拦截异步请求
+       // configurer.registerCallableInterceptors();
+        //configurer.registerDeferredResultInterceptors((DeferredResultProcessingInterceptor) myInterceptor);
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {

06f28d77a0bfe5e9b7d0d20588888143ed931965
rory-su <136471676@qq.com>
2019-06-02 09:45:15 +0800

    修改配置文件

--- security-demo/src/main/java/com/rory/controller/TestController.java
+++ security-demo/src/main/java/com/rory/controller/TestController.java
@@ -1,18 +1,25 @@
 package com.rory.controller;
 
-import com.rory.exception.UserNotExistException;
-import org.springframework.boot.autoconfigure.web.BasicErrorController;
+
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.codehaus.jackson.map.util.JSONPObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class TestController {
 
     @RequestMapping("/test")
     @ResponseBody
-    public String test(){
+    public String test(HttpServletResponse response, HttpServletRequest request){
+        String str = ReflectionToStringBuilder.toString(request);
        // throw  new UserNotExistException("123");
-        return "testee";
+        //JSONArray array = JSONArray.fromObject(list);
+        System.out.print(str);
+        return str;
     }
 }

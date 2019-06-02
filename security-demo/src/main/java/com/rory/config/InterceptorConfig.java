37defc6bbdac9e2d13d8c57e16f339cc98e1fef1
rory-su <136471676@qq.com>
2019-06-02 10:10:33 +0800

    修改配置文件

--- security-demo/src/main/java/com/rory/config/InterceptorConfig.java
+++ security-demo/src/main/java/com/rory/config/InterceptorConfig.java
@@ -1,9 +1,28 @@
 package com.rory.config;
 
+import com.rory.interceptor.MyInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class InterceptorConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    private MyInterceptor myInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(myInterceptor);
+
+        //添加多个拦截器
+        //registry.addInterceptor(otherintecptor);
+        // super.addInterceptors(registry);
+
+    }
+
+
+
 
 }
